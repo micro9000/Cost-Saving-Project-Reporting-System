3bb3c5d86c6aedd1eed4afb1c70c5b5a4bf968ef
Raniel Garcia <ranielgarcia2596@gmail.com>
2020-01-02 12:32:49 +0800

    Add new class for proposal status log repository

---
+++ Persistence/Repositories/ProposalStatusLogRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Repositories;
+using Domain;
+using Dapper;
+using DapperExtensions;
+using System.Linq;
+
+namespace Persistence.Repositories
+{
+	public class ProposalStatusLogRepository : Repository<ProposalStatusLog>, IProposalStatusLogRepository
+	{
+		public List<ProposalStatusLog> GetAllStatusLogByProposal (int proposalID)
+		{
+			List<ProposalStatusLog> results = new List<ProposalStatusLog>();
+
+			var pgMain = new PredicateGroup
+			{
+				Operator = GroupOperator.And,
+				Predicates = new List<IPredicate>()
+			};
+			pgMain.Predicates.Add(Predicates.Field<ProposalImg>(e => e.ProposalId, Operator.Eq, proposalID));
+			pgMain.Predicates.Add(Predicates.Field<ProposalImg>(e => e.IsDeleted, Operator.Eq, 0));
+
+			using (var conn = new WrappedDbConnection(GetOpenConnection()))
+			{
+				results = conn.GetList<ProposalStatusLog>(pgMain).ToList();
+				conn.Close();
+			}
+
+			return results;
+		}
+	}
+}
