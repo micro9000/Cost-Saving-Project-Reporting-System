d2d2fd3a88a8e7949f5fff12f91cf182ed175b9a
Raniel Garcia <ranielgarcia2596@gmail.com>
2020-01-02 12:24:47 +0800

    Add interface for Proposal status loggging repository

---
+++ Repositories/IProposalStatusLogRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using Domain;
+namespace Repositories
+{
+	public interface IProposalStatusLogRepository : IRepository<ProposalStatusLog>
+	{
+		List<ProposalStatusLog> GetAllStatusLogByProposal (int proposalID);
+	}
+}
