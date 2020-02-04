7967777346274164d816f4f67d9199cf5c5ea84c
Raniel Garcia <ranielgarcia2596@gmail.com>
2020-02-04 16:11:24 +0800

    Released config

---
+++ ESAVINGS-v1/Models/UserSession.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Domain;
+
+namespace ESAVINGS_v1.Models
+{
+	public class UserSession
+	{
+		public string ffid
+		{
+			get;
+			set;
+		}
+		public string fullName
+		{
+			get;
+			set;
+		}
+		public string email
+		{
+			get;
+			set;
+		}
+
+		public string department
+		{
+			get;
+			set;
+		}
+
+		public StaticData.UserTypes type
+		{
+			get;
+			set;
+		}
+
+		public bool isMaster
+		{
+			get;
+			set;
+		}
+
+		public bool isMultiPermission
+		{
+			get;
+			set;
+		}
+
+		public string mgrFFID
+		{
+			get;
+			set;
+		}
+
+		private string _password = "";
+		public string password
+		{
+			get
+			{
+				return this._password;
+			}
+
+			set
+			{
+				this._password = value;
+			}
+		}
+
+		private string costAnalystID;
+
+		public string CostAnalystID
+		{
+			get
+			{
+				return costAnalystID;
+			}
+			set
+			{
+				costAnalystID = value;
+			}
+		}
+
+		private string financeID;
+
+		public string FinanceID
+		{
+			get
+			{
+				return financeID;
+			}
+			set
+			{
+				financeID = value;
+			}
+		}
+
+		private bool isDL;
+
+		public bool IsDL
+		{
+			get
+			{
+				return isDL;
+			}
+			set
+			{
+				isDL = value;
+			}
+		}
+
+
+	}
+}
